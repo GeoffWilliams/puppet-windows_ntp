4649d9993a76b954e0b768b459ded0efb3c65460
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-06-01 07:23:11 +1000

    intial working version

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,48 +1,24 @@
-# Class: windows_ntp
-# ===========================
+# Windows_ntp
 #
-# Full description of class windows_ntp here.
+# Configure NTP on windows when not being managed by a windows domain, eg for
+# DMZs
 #
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'windows_ntp':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2017 Your name here, unless otherwise noted.
-#
-class windows_ntp {
+# @param server NTP server to sync time from
+class windows_ntp(
+    $server = "pool.ntp.org",
+) {
 
+  # https://stackoverflow.com/a/35626035/3441106
+  exec { "enable and configure windows ntp server":
+    command  => "w32tm /config /syncfromflags:manual /manualpeerlist:\"${server}\"",
+    unless   => "w32tm /query /peers | findstr ${server}",
+    notify   => Service["w32time"],
+    provider => powershell,
+    path     => 'c:/windows/system32',
+  }
 
+  service { "w32time":
+    ensure => running,
+    enable => true,
+  }
 }
